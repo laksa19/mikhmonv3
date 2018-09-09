ca72ffa6a00a71ec51e4cfa6209c8b7f386c20af
Laksamadi Guko <elru.lakusa@gmail.com>
2018-09-10 02:54:30 +0700

    Update routeros_api.class.php

--- lib/routeros_api.class.php
+++ lib/routeros_api.class.php
@@ -449,10 +449,87 @@ function decrypt($string, $key=128) {
 }
 
 // Reformat date time MikroTik
-
+/*
 function formatDTM($dtm){
-$val_conver = $dtm; 
-$new_format = str_replace("s", "", str_replace("m", ":", str_replace("h", ":", str_replace("d", "d ", str_replace("w", "w ", $val_conver)))));
+$val_convert = $dtm; 
+$new_format = str_replace("s", "", str_replace("m", ":", str_replace("h", ":", str_replace("d", "d ", str_replace("w", "w ", $val_convert)))));
 return $new_format;
+}
+*/
+function formatDTM($dtm){
+if(substr($dtm, 1,1) == "d" || substr($dtm, 1,1) == "w" || substr($dtm, 2,1) == "d" || substr($dtm, 2,1) == "w" ){
+    $day = explode("d",$dtm)[0]."d";
+    $day = str_replace("d", "d ", str_replace("w", "w ", $day));
+    $dtm = explode("d",$dtm)[1];
+    
+}
+// secs
+if(strlen($dtm) == "2" && substr($dtm, -1) == "s"){
+    $format = $day." 00:00:0".substr($dtm, 0,-1);
+}elseif(strlen($dtm) == "3" && substr($dtm, -1) == "s"){
+    $format = $day." 00:00:".substr($dtm, 0,-1);
+//minutes
+}elseif(strlen($dtm) == "2" && substr($dtm, -1) == "m"){
+    $format = $day." 00:0".substr($dtm, 0,-1).":00";
+}elseif(strlen($dtm) == "3" && substr($dtm, -1) == "m"){
+    $format = $day." 00:".substr($dtm, 0,-1).":00";
+//hours
+}elseif(strlen($dtm) == "2" && substr($dtm, -1) == "h"){
+    $format = $day." 0".substr($dtm, 0,-1).":00:00";
+}elseif(strlen($dtm) == "3" && substr($dtm, -1) == "h"){
+    $format = $day." ".substr($dtm, 0,-1).":00:00";          
+ 
+//minutes -secs 
+}elseif(strlen($dtm) == "4" && substr($dtm, -1) == "s" && substr($dtm,1,-2) == "m"){
+    $format = $day." "."00:0".substr($dtm, 0,1).":0".substr($dtm, 2,-1);
+}elseif(strlen($dtm) == "5" && substr($dtm, -1) == "s" && substr($dtm,1,-3) == "m"){
+    $format = $day." "."00:0".substr($dtm, 0,1).":".substr($dtm, 2,-1);  
+}elseif(strlen($dtm) == "5" && substr($dtm, -1) == "s" && substr($dtm,2,-2) == "m"){
+    $format = $day." "."00:".substr($dtm, 0,2).":0".substr($dtm, 3,-1);
+}elseif(strlen($dtm) == "6" && substr($dtm, -1) == "s" && substr($dtm,2,-3) == "m"){
+    $format = $day." "."00:".substr($dtm, 0,2).":".substr($dtm, 3,-1);
+
+//hours -secs 
+}elseif(strlen($dtm) == "4" && substr($dtm, -1) == "s" && substr($dtm,1,-2) == "h"){
+    $format = $day." 0".substr($dtm, 0,1).":00:0".substr($dtm, 2,-1);
+}elseif(strlen($dtm) == "5" && substr($dtm, -1) == "s" && substr($dtm,1,-3) == "h"){
+    $format = $day." 0".substr($dtm, 0,1).":00:".substr($dtm, 2,-1);  
+}elseif(strlen($dtm) == "5" && substr($dtm, -1) == "s" && substr($dtm,2,-2) == "h"){
+    $format = $day." ".substr($dtm, 0,2).":00:0".substr($dtm, 3,-1);
+}elseif(strlen($dtm) == "6" && substr($dtm, -1) == "s" && substr($dtm,2,-3) == "h"){
+    $format = $day." ".substr($dtm, 0,2).":00:".substr($dtm, 3,-1);
+
+//hours -secs 
+}elseif(strlen($dtm) == "4" && substr($dtm, -1) == "m" && substr($dtm,1,-2) == "h"){
+    $format = $day." 0".substr($dtm, 0,1).":0".substr($dtm, 2,-1).":00";
+}elseif(strlen($dtm) == "5" && substr($dtm, -1) == "m" && substr($dtm,1,-3) == "h"){
+    $format = $day." 0".substr($dtm, 0,1).":".substr($dtm, 2,-1).":00";
+}elseif(strlen($dtm) == "5" && substr($dtm, -1) == "m" && substr($dtm,2,-2) == "h"){
+    $format = $day." ".substr($dtm, 0,2).":0".substr($dtm, 3,-1).":00";
+}elseif(strlen($dtm) == "6" && substr($dtm, -1) == "m" && substr($dtm,2,-3) == "h"){
+    $format = $day." ".substr($dtm, 0,2).":".substr($dtm, 3,-1).":00";
+
+//hours minutes secs
+}elseif(strlen($dtm) == "6" && substr($dtm, -1) == "s" && substr($dtm,3,-2) == "m" && substr($dtm,1,-4) == "h"){    
+    $format = $day." 0".substr($dtm, 0,1).":0".substr($dtm, 2,-3).":0".substr($dtm, 4,-1);
+}elseif(strlen($dtm) == "7" && substr($dtm, -1) == "s" && substr($dtm,3,-3) == "m" && substr($dtm,1,-5) == "h"){    
+    $format = $day." 0".substr($dtm, 0,1).":0".substr($dtm, 2,-4).":".substr($dtm, 4,-1);
+}elseif(strlen($dtm) == "7" && substr($dtm, -1) == "s" && substr($dtm,4,-2) == "m" && substr($dtm,1,-5) == "h"){    
+    $format = $day." 0".substr($dtm, 0,1).":".substr($dtm, 2,-3).":0".substr($dtm, 5,-1);
+}elseif(strlen($dtm) == "8" && substr($dtm, -1) == "s" && substr($dtm,4,-3) == "m" && substr($dtm,1,-6) == "h"){    
+    $format = $day." 0".substr($dtm, 0,1).":".substr($dtm, 2,-4).":".substr($dtm, 5,-1);         
+}elseif(strlen($dtm) == "7" && substr($dtm, -1) == "s" && substr($dtm,4,-2) == "m" && substr($dtm,2,-4) == "h"){    
+    $format = $day." ".substr($dtm, 0,2).":0".substr($dtm, 3,-3).":0".substr($dtm, 5,-1);    
+}elseif(strlen($dtm) == "8" && substr($dtm, -1) == "s" && substr($dtm,4,-3) == "m" && substr($dtm,2,-5) == "h"){    
+    $format = $day." ".substr($dtm, 0,2).":0".substr($dtm, 3,-4).":".substr($dtm, 5,-1); 
+}elseif(strlen($dtm) == "8" && substr($dtm, -1) == "s" && substr($dtm,5,-2) == "m" && substr($dtm,2,-5) == "h"){    
+    $format = $day." ".substr($dtm, 0,2).":".substr($dtm, 3,-3).":0".substr($dtm, 6,-1);
+}elseif(strlen($dtm) == "9" && substr($dtm, -1) == "s" && substr($dtm,5,-3) == "m" && substr($dtm,2,-6) == "h"){    
+    $format = $day." ".substr($dtm, 0,2).":".substr($dtm, 3,-4).":".substr($dtm, 6,-1);  
+
+}else{
+    $format = $dtm;
+}
+return $format;
 }
 ?>

2fe9f1dcec501d27578cfb99ce28525b41eb29a5
Laksamadi Guko <elru.lakusa@gmail.com>
2019-02-07 09:45:15 +0700

    Add files via upload

--- admin.php
+++ admin.php
@@ -78,16 +78,13 @@ if ($id == "login" || substr($url, -1) == "p") {
     if ($user == $useradm && $pass == decrypt($passadm)) {
       $_SESSION["mikhmon"] = $user;
 
-      if ($expiration_date > $today) {
         echo "<script>window.location='./admin.php?id=sessions'</script>";
-      } else {
-        $error = '<div style="width: 100%; padding:5px 0px 5px 0px; border-radius:5px;" class="bg-danger"></i> Alert!<br>Mikhmon Online Expired ' . $exp_date . '</div><div style="width: 100%; padding:5px 0px 5px 0px; border-radius:5px;">Perpanjang akun <a href="#infoexp">klik di sini</a> </div>';
-      }
-
+    
     } else {
       $error = '<div style="width: 100%; padding:5px 0px 5px 0px; border-radius:5px;" class="bg-danger"><i class="fa fa-ban"></i> Alert!<br>Invalid username or password.</div>';
     }
   }
+  
 
   include_once('./include/login.php');
 } elseif (!isset($_SESSION["mikhmon"])) {

f3abfb5f373ac81906972b4d2729ed4a400f0954
Laksamadi Guko <elru.lakusa@gmail.com>
2019-04-04 07:45:03 +0700

    Add files via upload

--- include/version.php
+++ include/version.php
@@ -2,7 +2,7 @@
 if (!isset($_SESSION["mikhmon"])) {
     header("Location:../admin.php?id=login");
   } else {
-        $_SESSION["v"] = "3.13 r6 04-01-2019";
+        $_SESSION["v"] = "3.13 r6 04-02-2019";
         //echo '<span style="display:none" id="ver">3136</span>';
     
     }
